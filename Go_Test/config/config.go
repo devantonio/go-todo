7b5e001ccec4a4b911e237f9a1e5f51ecf75938d
devantonio <antonio.ae91@gmail.com>
2024-07-29 21:44:34 -0400

    updated DB config

--- Go_Test/config/config.go
+++ Go_Test/config/config.go
@@ -4,24 +4,24 @@ import "os"
 
 // Config holds the application configuration.
 type Config struct {
-    Port   string
-    DBURI  string
-    DBName string
+	Port   string
+	DBURI  string
+	DBName string
 }
 
 // LoadConfig loads configuration from environment variables.
 func LoadConfig() Config {
-    return Config{
-        Port:   getEnv("PORT", "8080"),
-        DBURI:  getEnv("DB_URI", "mongodb://localhost:27017"),
-        DBName: getEnv("DB_NAME", "GO_DB"), // Default to "testdb" if not set
-    }
+	return Config{
+		Port:   getEnv("PORT", "8080"),
+		DBURI:  getEnv("DB_URI", "mongodb://localhost:27017"),
+		DBName: getEnv("DB_NAME", "GO_DB"), // Default to "testdb" if not set
+	}
 }
 
 // getEnv retrieves an environment variable or returns a default value.
 func getEnv(key, defaultValue string) string {
-    if value, exists := os.LookupEnv(key); exists {
-        return value
-    }
-    return defaultValue
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
 }
