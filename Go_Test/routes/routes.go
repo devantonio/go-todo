54ca3eafbb18ae129a5ba6dd09e0bba09596a5d5
devantonio <antonio.ae91@gmail.com>
2024-07-29 21:57:44 -0400

    Update main.go

--- Go_Test/routes/routes.go
+++ Go_Test/routes/routes.go
@@ -1,23 +1,37 @@
 package routes
 
 import (
-    "net/http"
-	"log"
-    "github.com/gorilla/mux"
-    "go_test/controllers"
+	"go_test/controllers"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // RegisterRoutes sets up the routes for the application.
 func RegisterRoutes(router *mux.Router, taskController *controllers.TaskController) {
-    // Serve static files from the /static directory
-    router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+	// Serve static files from the /static directory
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Serving index.html")
-		http.ServeFile(w, r, "static/templates/index.html")
+		// Fetch data from the database or any other source
+		tasks, err := taskController.GetTasks(2)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Parse and execute the template
+		tmpl, err := template.ParseFiles("static/templates/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		tmpl.Execute(w, tasks)
 	})
-	
-    // API routes
-    router.HandleFunc("/tasks", taskController.GetTasks).Methods("GET")
-    router.HandleFunc("/tasks", taskController.CreateTask).Methods("POST")
+
+	// API routes
+	router.HandleFunc("/tasks", taskController.GetTask).Methods("GET")
+	router.HandleFunc("/tasks", taskController.CreateTask).Methods("POST")
 }
